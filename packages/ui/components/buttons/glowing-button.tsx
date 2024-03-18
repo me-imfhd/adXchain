06434d20b324034b0595d65db44dcf0f79920297
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-03-18 21:11:15 +0530

    custom reusable btn comps  action,glow,zoom

---
+++ packages/ui/components/buttons/glowing-button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, ButtonProps } from "../button";
+import { cn } from "../../cn";
+
+export function GlowingButton({
+  children,
+  className,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps) {
+  return (
+    <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#3178c6]">
+      <Button
+        {...props}
+        className={cn(
+          className,
+          "fancy-border-gradient hover:bg-background relative mx-auto flex gap-4 border-none"
+        )}
+        variant="outline"
+      >
+        {children}
+      </Button>
+    </div>
+  );
+}
---
+++ packages/ui/components/buttons/index.ts
@@ -0,0 +1,3 @@
+export * from "./action-button";
+export * from "./glowing-button";
+export * from "./zooming-button";
