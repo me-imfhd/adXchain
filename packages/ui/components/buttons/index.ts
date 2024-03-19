4c3ede8c58bd7c8e2f3b13e7bf97dfec27f31c20
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-03-19 16:30:25 +0530

    new button component

--- packages/ui/components/buttons/index.ts
+++ packages/ui/components/buttons/index.ts
@@ -1,3 +1,4 @@
 export * from "./action-button";
 export * from "./glowing-button";
 export * from "./zooming-button";
+export * from "./shiny-button";
---
+++ packages/ui/components/buttons/shiny-button.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, ButtonProps } from "../button";
+import { cn } from "../../cn";
+import { buttonVariants } from "../button-variants";
+
+export function ShinyButton({
+  children,
+  animationType,
+  size,
+  tap,
+  variant,
+  className,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps) {
+  return (
+    <Button
+      asChild
+      {...props}
+      className={cn(
+        buttonVariants({ animationType, className, size, tap, variant }),
+        "hero-join-button-dark rounded-xl relative overflow-hidden  p-[1px] font-bold transition-all duration-300 hover:shadow-[0_0_2rem_-0.5rem_#fff8] "
+      )}
+      variant="outline"
+    >
+      <span className="text-foreground px-3 py-2 transition-all duration-300 bg-background ">
+        {children}
+      </span>
+    </Button>
+  );
+}
