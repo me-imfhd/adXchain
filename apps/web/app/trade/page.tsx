400ebf2e3ea2ff12eb6608180f52ea2f2cb16eb2
Prashant Varma <prashantvarma5083@gmail.com>
2024-03-27 19:48:59 +0530

    added FAQs and more

--- apps/web/app/trade/page.tsx
+++ apps/web/app/trade/page.tsx
@@ -1,15 +1,15 @@
 import Trade from "@/components/landing-page/Trade";
 import React from "react";
 
-export default function TradePage(){
-    return (
-        <div
+export default function TradePage() {
+  return (
+    <div
       style={{
         backgroundImage: "radial-gradient( #666, #444, #111111, #010101)",
       }}
-      className="flex flex-col items-center justify-center  w-full"
+      className="flex flex-col items-center justify-center mt-16  w-full"
     >
-      <Trade/>
+      <Trade />
     </div>
-    )
+  );
 }
