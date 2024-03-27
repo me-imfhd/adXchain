e8d3fdfe81a9282fcaf312df84018e86c3972c7f
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-03-27 21:20:49 +0530

    rename

---
+++ packages/api/lib/umi.ts
@@ -0,0 +1,3 @@
+export { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+export { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+export { mplCore } from "@metaplex-foundation/mpl-core";
