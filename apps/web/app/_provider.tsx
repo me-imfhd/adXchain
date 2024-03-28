08850fcc8d4ce0d09c357bcf72f33d8793caf54a
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-03-29 04:46:36 +0530

    add react query and react hook form

--- apps/web/app/_provider.tsx
+++ apps/web/app/_provider.tsx
@@ -6,14 +6,19 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 import { useMemo, type PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const Provider = ({ children }: PropsWithChildren) => {
   const wallets = useMemo(() => [], []);
+  const queryClient = new QueryClient();
+
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <WalletProvider wallets={wallets} autoConnect>
         {/* <TrpcProvider> */}
-        <WalletModalProvider>{children}</WalletModalProvider>
+        <QueryClientProvider client={queryClient}>
+          <WalletModalProvider>{children}</WalletModalProvider>
+        </QueryClientProvider>
         {/* </TrpcProvider> */}
       </WalletProvider>
     </ThemeProvider>
