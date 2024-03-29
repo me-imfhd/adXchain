f0fded450fee018fc7c9b2f53fd9bb2db211c31f
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-03-29 19:40:01 +0530

    update delegate

--- apps/web/app/test/page.tsx
+++ apps/web/app/test/page.tsx
@@ -5,18 +5,20 @@ import {
   createNFTForCollection,
   fetchAssestByCollection,
   fetchCollection,
+  UpdateDelegatePlug,
 } from "@repo/api";
 import React, { useState } from "react";
 
 import { PublicKey, generateSigner, publicKey } from "@repo/api/lib/umi";
 import { Button } from "@repo/ui/components";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 const COLLECTION = publicKey("2y9izbXnf7VPJpp9vbL4g5cqctS3h4aRaBA8QDfzfQvq");
 const uri = "https://arweave.net/b56MPdHptYzhLlyjIBB-FM9KO66KYcDEkJFc2qFJSYo";
 export default function Page() {
   const umi = useUmi();
   const [collection, setCollection] = useState<PublicKey>(COLLECTION);
-
+  const wallet = useWallet();
   if (!collection) {
     alert("Create a Collection First");
   }
@@ -74,6 +76,23 @@ export default function Page() {
         }}
       >
         Log All NFT
+      </Button>
+      <Button
+        onClick={async () => {
+          const { sign } = await UpdateDelegatePlug({
+            umi,
+            collection,
+            assetAddress: publicKey(
+              "AmnjRE9ytrPmiRKP6zFPn16qF3XaHL3AjiNnbeQMxxtL"
+            ),
+            newAuthor: publicKey(
+              "3KPDxKq9AfRwC9GTcao1Uw1Q2DwaM2FWCnMSnwkTAaHm"
+            ),
+          });
+          console.log(sign);
+        }}
+      >
+        Enable Update Delegate
       </Button>
     </div>
   );
--- packages/api/web3-ops/sell.ts
+++ packages/api/web3-ops/sell.ts
@@ -1,50 +1,61 @@
-import { addPluginV1, createPlugin } from "@metaplex-foundation/mpl-core";
+import {
+  PluginType,
+  addPluginV1,
+  addressPluginAuthority,
+  createPlugin,
+  revokePluginAuthorityV1,
+} from "@metaplex-foundation/mpl-core";
 import { Pda, PublicKey } from "@metaplex-foundation/umi";
 import { UmiInstance } from ".";
+import { base58 } from "@metaplex-foundation/umi/serializers";
 interface TransferDelegatePlug extends UmiInstance {
-  assetAddress: PublicKey | Pda;
-  collectionAddress?: PublicKey;
+  assetAddress: PublicKey;
+  collection: PublicKey;
+  newAuthor: PublicKey
 }
-export async function transferDelegatePlug({
+export async function UpdateDelegatePlug({
   assetAddress,
-  collectionAddress,
+  collection,
   umi,
+  newAuthor,
 }: TransferDelegatePlug) {
   const response = await addPluginV1(umi, {
     asset: assetAddress,
-    plugin: createPlugin({ type: "TransferDelegate" }),
-    collection: collectionAddress,
+    plugin: createPlugin({ type: "UpdateDelegate" }),
+    collection: collection,
+    initAuthority: addressPluginAuthority(newAuthor)
   }).sendAndConfirm(umi);
-
+  const signature = base58.deserialize(response.signature)[0];
   if (response.result.value.err) {
     throw new Error(
       (response.result.value.err as Error).message ??
-        `Error occured while delegating the NFT, txn sign: ${response.signature.toString()}`
+        `Error occured while delegating the NFT, txn sign: ${signature}`
     );
   }
   return {
-    sign: response.signature.toString(),
+    sign: signature,
   };
 }
 
-export async function freezeDelegatePlug({
+export async function revokeUpdatePlug({
   assetAddress,
-  collectionAddress,
+  collection,
   umi,
 }: TransferDelegatePlug) {
-  const response = await addPluginV1(umi, {
+  const response = await revokePluginAuthorityV1(umi, {
+    pluginType: PluginType.UpdateDelegate,
     asset: assetAddress,
-    plugin: createPlugin({ type: "FreezeDelegate", data: { frozen: true } }),
-    collection: collectionAddress,
+    collection,
   }).sendAndConfirm(umi);
+  const signature = base58.deserialize(response.signature)[0];
 
   if (response.result.value.err) {
     throw new Error(
       (response.result.value.err as Error).message ??
-        `Error occured while delegating the NFT, txn sign: ${response.signature.toString()}`
+        `Error occured while revoking the update delegate plug, txn sign: ${signature}`
     );
   }
   return {
-    sign: response.signature.toString(),
+    sign: signature,
   };
 }
