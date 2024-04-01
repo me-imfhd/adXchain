a7190fbbf20a548d68b19798dda5213f1a434151
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-04-01 07:01:03 +0530

    update next-auth package

---
+++ packages/auth/index.ts
@@ -0,0 +1,47 @@
+import { db } from "@repo/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { DefaultSession, getServerSession, NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import { redirect } from "next/navigation";
+
+declare module "next-auth" {
+  interface Session {
+    user: DefaultSession["user"] & {
+      id: string;
+    };
+  }
+}
+
+export type AuthSession = {
+  session: {
+    user: {
+      id: string;
+      name?: string;
+      email?: string;
+    };
+  } | null;
+};
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  debug: process.env.NODE_ENV !== "production",
+  callbacks: {
+    session: ({ session, user }) => {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  providers: [],
+};
+
+export const getUserAuth = async () => {
+  const session = await getServerSession(authOptions);
+  return { session } as AuthSession;
+};
+
+export const checkAuth = async () => {
+  const { session } = await getUserAuth();
+  if (!session) redirect("/api/auth/signin");
+};
+
+export {SessionProvider} from "next-auth/react"
