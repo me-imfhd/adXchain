6b457177583cf56ded1f99ffaa4058b3e8597abb
Prashant Varma <prashantvarma5083@gmail.com>
2024-04-03 04:25:32 +0530

    ,,

--- apps/web/components/layout/NFTCollectionCard.tsx
+++ apps/web/components/layout/NFTCollectionCard.tsx
@@ -6,9 +6,10 @@ interface NFTCollectionCardProps{
     imgURL: any;
     CollectionName: string;
     CollectionWebsite: string
+    CollectionPlatFrom?:string
 }
 
-const NFTCollectionCard:React.FC<NFTCollectionCardProps> = ({imgURL, CollectionName, CollectionWebsite}) => {
+const NFTCollectionCard:React.FC<NFTCollectionCardProps> = ({imgURL, CollectionName, CollectionWebsite, CollectionPlatFrom}) => {
  return (
     <div className=' p-4 border-2 border-[#C2C1FF]  rounded-lg'>
         <div className='h-56 w-full relative'>
@@ -21,8 +22,9 @@ const NFTCollectionCard:React.FC<NFTCollectionCardProps> = ({imgURL, CollectionN
             />
         </div>
         <div className='flex flex-col mt-4 items-start justify-between'>
-            <span className='text-white font-semibold text-xl'>{CollectionName}'s Collection</span>
-        <Link href={`${CollectionWebsite}`} className='text-blue-400 hover:underline'>{CollectionWebsite}</Link>
+            <span className='text-white text-xl'>Name: {" "} <span className='font-semibold'>{CollectionName}</span></span>
+        <span>Website: {" "}<Link href={`${CollectionWebsite}`} className='text-blue-400 hover:underline'>{CollectionWebsite}</Link></span>
+        {CollectionPlatFrom && <span>Platform: {" "}<Link href={`${CollectionPlatFrom}`} className='text-blue-300 hover:underline'>{CollectionPlatFrom}</Link></span>}
         </div>
     </div>
  )
