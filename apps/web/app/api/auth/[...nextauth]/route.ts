7dc260159ce099c249ad2cf28c059cfa30e35001
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-04-04 17:07:10 +0530

    next auth issue fix

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
-import { handler } from "@repo/auth";
+import { authOptions } from "@repo/auth";
+import NextAuth from "@repo/auth/next";
+
+const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
