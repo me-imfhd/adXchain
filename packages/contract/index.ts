e4fbfe662c8656f87d2d957859727378df3e65cd
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-04-16 19:30:44 +0530

    implmenting anchor program and anchor to frontend

--- packages/contract/index.ts
+++ packages/contract/index.ts
@@ -1,1 +1,9 @@
+import { Program } from "@coral-xyz/anchor";
+import contract from "./target/idl/contract.json";
+import { Contract } from "./target/types/contract";
+
+export * as anchor from "@coral-xyz/anchor";
 export * from "./target/types/contract";
+export const anchorProgram = anchor.workspace.Contract as Program<Contract>;
+
+export default contract;
