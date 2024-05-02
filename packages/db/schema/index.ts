03d5f6c398590d3a177b51233ced191d8664de65
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-05-02 16:31:31 +0530

    remove unused code

--- packages/db/schema/index.ts
+++ packages/db/schema/index.ts
@@ -1,11 +1,7 @@
 import { z } from "zod";
 
-export * from "./imageConversion";
-export * from "./auth";
 export * from "./adSlots";
 export * from "./inventory";
-export * from "./user";
-export * from "./project";
 export const getAdsSchema = z.object({
   inventoryId: z.string(),
   underdogApiEndpoint: z.string(),
